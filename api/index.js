c42da52fc270172cb2c6501fb8c6efbc48ba49b3
KMCR41207 <qwertyasd077a@gmail.com>
2026-05-19 09:45:02 +0530

    Clean terminal: nodemon --quiet, suppress deprecation warnings

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+process.removeAllListeners('warning'); // suppress deprecation warnings
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
